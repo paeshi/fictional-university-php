2eeddd20cd665e1c2099f4d2ddef23167ea0fc71
stevepae <stevepaemusic@gmail.com>
2021-06-23 22:29:21 -0400

    index.php
    -add specific wp loop with wp functions

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
-$names = array("Steve", "Jane", "Bob");
-$count = 0;
-while($count < count($names)){
-    echo "<li> Hi, I'm $names[$count]</li>";
-    $count++;
-}
+
+while(have_posts()){
+    the_post(); ?>
+    <h2><a href="<?php the_permalink();?>"> <?php the_title(); ?> </a></h2>
+    <?php the_content(); ?>
+    <hr>
+<?php }
     
 ?>
-<p>Hi, I'm <?php echo $names[1]; ?>.</p>
+
 
